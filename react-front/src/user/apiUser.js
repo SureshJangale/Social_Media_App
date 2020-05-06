efb00d23546b956949c1b1d7ffeeba0d038e5e70
Suresh Jangale <sureshjangale137@gmail.com>
2020-05-06 19:48:25 +0530

    Adding node-api and react-front-end for user routes.

---
+++ react-front/src/user/apiUser.js
@@ -0,0 +1,58 @@
+export const read = (userId, token) => {
+    return  fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+         method: "GET",
+         headers: {
+             Accept: "application/json",
+             "Content-type": "application/json",
+             Authorization: `Bearer ${token}`
+         }
+     })
+         .then(response => {
+             return response.json()
+         })
+         .catch(err => console.log(err))
+ }
+
+ export const update = (userId, token, user) => {
+     console.log(user);
+     
+    return  fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+         method: "PUT",
+         headers: {
+             Accept: "application/json",
+             Authorization: `Bearer ${token}`
+         },
+         body:user
+     })
+         .then(response => {
+             return response.json()
+         })
+         .catch(err => console.log(err))
+ }
+ 
+ export const remove = (userId, token) => {
+    return  fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+         method: "DELETE",
+         headers: {
+             Accept: "application/json",
+             "Content-type": "application/json",
+             Authorization: `Bearer ${token}`
+         }
+     })
+         .then(response => {
+             return response.json()
+         })
+         .catch(err => console.log(err))
+ }
+
+
+ export const list = () => {
+    return  fetch(`${process.env.REACT_APP_API_URL}/users`, {
+         method: "GET",
+        })
+         .then(response => {
+             return response.json()
+         })
+         .catch(err => console.log(err)
+         )
+ }
